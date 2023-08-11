bce06ef8f08d0084c4db628c1a4fb8a3145331ed
Leonardo A. Alvarenga <leosgalarenga@protonmail.com>
2023-08-11 15:52:06 -0300

    feat: create subheader component

---
+++ src/components/Subheader.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let id = '';
+</script>
+
+<h3
+    {id}
+    class={`
+        font-major text-lg md:text-xl
+        relative font-bold before:bottom-0
+        before:h-[30%] before:w-full
+        before:absolute before:left-0
+        before:bg-opacity-70 before:transition-all
+        before:duration-500 before:-z-10
+    `}
+>
+    <slot></slot>
+</h3>
