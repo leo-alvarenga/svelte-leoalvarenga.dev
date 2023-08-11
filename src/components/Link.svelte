7699f43440575097f8aaf08a77e620df74e035ce
Leonardo A. Alvarenga <leosgalarenga@protonmail.com>
2023-08-11 15:51:49 -0300

    feat: link prop n width change

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
     export let href = '/';
     export let className = '';
+    export let sameTab = false;
 </script>
 
 <a
     class={`
-        px-1 ${className}
+        px-1 w-fit
         relative hover:before:h-full hover:text-background
+        active:before:h-full active:text-background
         transition-all duration-500 before:h-[10%]
         before:w-full before:absolute
         before:left-0 before:bottom-0
         before:bg-primary before:-z-10
         before:transition-all before:duration-500
+        ${className}
     `}
-    target="_blank"
+    target={sameTab ? undefined : '_blank'}
     {href}
 >
     <slot></slot>
