028979eafd9663aa5f3a9fce91ffaaa5581d0fcf
Leonardo A. Alvarenga <leosgalarenga@protonmail.com>
2023-08-15 00:31:32 -0300

    feat: contact area

--- src/components/Home/Contact/Contact.svelte
+++ src/components/Home/Contact/Contact.svelte
@@ -1,16 +1,26 @@
 <script>
+    import { contacts } from "$lib";
+	import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
 	import Header from "../../Header.svelte";
-import Link from "../../Link.svelte";
-
+    import Subheader from "../../Subheader.svelte";
+	import Link from "../../Link.svelte";
+	import ContactCard from "./ContactCard.svelte";
+	import Profile from "./Profile.svelte";
 </script>
 
-<Header id="contact">Contact</Header>
+<Header id="contact">Contact me!</Header>
 
-<div
-    class={`
-        flex flex-col lg:flex-row
-        w-full items-center
-    `}
->
-    <Link href="aaa">aaa</Link>
+<Profile />
+
+<div class="flex flex-row flex-wrap justify-center w-[80%] items-center gap-2 text-lg md:text-xl">
+    {#each contacts as { href, icon, title }}
+        <Link className="!p-4 min-w-fit flex-1 flex flex-row items-center gap-4" {href}>
+            <Fa {icon} />
+            <Subheader>
+                {title}
+            </Subheader>
+            <Fa class="ml-auto" icon={faArrowUpRightFromSquare} />
+        </Link>
+    {/each}
 </div>
---
+++ src/components/Home/Contact/Profile.svelte
@@ -0,0 +1,36 @@
+<span
+    class={`
+            border-pic group
+            relative w-64 transition-all
+            before:absolute before:left-2
+            before:top-2 before:opacity-20
+            before:w-64 before:h-64
+            before:-z-10
+            before:animate-blob
+    
+            after:absolute after:-left-2
+            after:bottom-2 after:opacity-20
+            after:w-64 after:h-64 after:animate-blobAlt
+            after:-z-20
+    `}
+>
+    <img
+        class="border-pic animate-blob"
+        src="https://avatars.githubusercontent.com/u/49699932?v=4"
+        alt="Profile"
+    />
+
+    <span class="absolute hidden group-hover:visible">a</span>
+</span>
+
+<style lang="postcss">
+    .border-pic {
+        -webkit-shape-outside:circle();
+        shape-outside:circle();
+    }
+
+    .border-pic::before, .border-pic::after {
+        background: theme(colors.primary);
+        background: linear-gradient(90deg, theme(colors.primary) 0%, theme(colors.blueLight) 100%);
+    }
+</style>
