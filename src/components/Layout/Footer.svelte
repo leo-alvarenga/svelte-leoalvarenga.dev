60dab1a3c373fcc006bc56eaeae1e38434566cca
Leonardo A. Alvarenga <leosgalarenga@protonmail.com>
2023-08-18 16:53:57 -0300

    feat: add footer bg

--- src/components/Layout/Footer.svelte
+++ src/components/Layout/Footer.svelte
@@ -1,8 +1,10 @@
 <footer
     class={`
-        mt-auto
+        mt-auto relative
         flex flex-col lg:flex-row items-center
-        justify-center py-4 w-full relative
+        justify-center py-4 w-full
+        bg-light bg-opacity-40 border-t border-t-light
+        backdrop-filter backdrop-blur-lg
     `}
 >
     <h4>Built with ❤️, using SvelteKit and TailwindCSS</h4>
