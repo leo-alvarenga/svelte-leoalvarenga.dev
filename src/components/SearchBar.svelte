d357ba4856643dba9d6e6e30ad6acc60c3986b9d
Leonardo A. Alvarenga <leosgalarenga@protonmail.com>
2023-08-21 23:21:31 -0300

    feat: search bar component

---
+++ src/components/SearchBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { faSearch, faSpinner } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+
+    export let busy = false;
+    export let onChange: (val: string) => void;
+
+    let value = '';
+
+    $: onChange && onChange(value);
+</script>
+
+<div class="relative justify-items-center flex flex-row gap-2 items-center bg-background px-2 py-1 border border-primary rounded-md">
+    <Fa class={`opacity-40 ${busy ? 'animate-spin' : ''}`} icon={busy ? faSpinner : faSearch} />
+
+    <input
+        bind:value
+        placeholder="Type in..."
+        type="text"
+        name="searchbar"
+        class={`
+            text-text bg-background 
+            outline-none
+        `}
+    />
+</div>
