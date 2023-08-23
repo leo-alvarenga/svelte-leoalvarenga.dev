5fe53160146359f82661ee07709edee8b6b2f8e8
Leonardo A. Alvarenga <leosgalarenga@protonmail.com>
2023-08-23 16:50:27 -0300

    feat: searchbar component lifecycle

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,26 +1,45 @@
 <script lang="ts">
 	import { faSearch, faSpinner } from "@fortawesome/free-solid-svg-icons";
 	import Fa from "svelte-fa";
+	import { t } from "../store";
 
     export let busy = false;
+    export let placeholder = '';
+    export let className = '';
     export let onChange: (val: string) => void;
+
+    let border = 'border-text';
 
     let value = '';
 
-    $: onChange && onChange(value);
+    function applyBorder(focus?: boolean) {
+        border = focus ? 'border-primary' : 'border-text'
+    }
 </script>
 
-<div class="relative justify-items-center flex flex-row gap-2 items-center bg-background px-2 py-1 border border-primary rounded-md">
+<div
+    class={`
+        relative justify-items-center
+        flex flex-row gap-2 items-center
+        bg-background px-2 py-1 border
+        border-primary rounded-md
+        ${border} ${className}
+    `}
+>
     <Fa class={`opacity-40 ${busy ? 'animate-spin' : ''}`} icon={busy ? faSpinner : faSearch} />
 
     <input
         bind:value
-        placeholder="Type in..."
         type="text"
         name="searchbar"
+        placeholder={$t(placeholder)}
+        on:focus={() => applyBorder(true)}
+        on:focusout={() => applyBorder()}
+        on:input={() => onChange(value)}
         class={`
             text-text bg-background 
-            outline-none
+            outline-none self-stretch
+            w-full
         `}
     />
 </div>
