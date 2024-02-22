dd2d5e00b03a949739024f1379f86413962d8f5b
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2024-02-22 20:49:55 -0300

    chore: update skills

--- src/lib/static/skills.ts
+++ src/lib/static/skills.ts
@@ -1,7 +1,11 @@
 export const skills = [
-    'HTML', 'CSS', 'JavaScript',
-    'TypeScript', 'React', 'Svelte',
-    'SvelteKit', 'Linux', 'SQL', 'Postgresql',
-    'styled-components', 'TailwindCSS',
-    'Docker', 'Bash'
+    'HTML', 'CSS', 'JavaScript', 'TypeScript',
+    'Vite', 'React', 'Redux', 'Next.js',
+    'Svelte', 'SvelteKit',
+    'MUI', 'styled-components', 'TailwindCSS',
+    'jest', 'vitest', 'Lua',
+    'Go', 'Bun', 'Elysia.js',
+    'Node.js', 'Express', 'Nest.js',
+    'SQL', 'Postgresql',
+    'Docker', 'Linux', 'Bash'
 ];
