b8632fba87f94b72547cd43d711981f8870cec3d
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2024-03-05 03:36:08 -0300

    feat: add title area to newest page

---
+++ src/components/More/TitleArea.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { t } from "../../store";
+</script>
+
+<div class="w-[90%] lg:w-[70%] text-center mb-4">
+    <h1 class="mt-10 mb-4 font-major text-2xl md:text-6xl">
+        {$t('page.more.title')}
+    </h1>
+    <h3 class="text-xl opacity-40">{$t('page.more.subtitle')}</h3>
+</div>
--- src/routes/more/+page.svelte
+++ src/routes/more/+page.svelte
@@ -1,5 +1,9 @@
 <script>
     import PlaylistArea from '../../components/More/PlaylistArea.svelte';
+	import MediaList from '../../components/More/MediaList.svelte';
+	import TitleArea from '../../components/More/TitleArea.svelte';
 </script>
 
+<TitleArea />
 <PlaylistArea />
+<MediaList />
