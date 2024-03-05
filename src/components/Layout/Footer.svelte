dc5acd1c8f74ac4fd32d3846ef0c200a8e9f9340
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2024-03-05 03:34:30 -0300

    fix: small layout issues fixed

--- src/components/Layout/Footer.svelte
+++ src/components/Layout/Footer.svelte
@@ -1,19 +1,18 @@
 <script>
     import { version } from '$app/environment';
     import { t } from "../../store";
-
 </script>
 
 <footer
     class={`
         mt-auto relative
         flex flex-col flex-wrap lg:flex-row items-center
-        justify-center py-4 w-full
+        justify-center p-4 w-full
         bg-light bg-opacity-40 border-t border-t-light
         backdrop-filter backdrop-blur-lg
     `}
 >
-    <h4>{$t('layout.footer.text')}</h4>
+    <h4 class="text-center">{$t('layout.footer.text')}</h4>
 
     <span class="absolute right-2 bottom-1 text-sm opacity-40">
         {version}
